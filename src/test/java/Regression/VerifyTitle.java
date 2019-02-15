ceac2bba6dd26b25a074194bdddfb0f459062ee1
realanandvyas <anand.x.vyas@gmail.com>
2019-02-15 15:07:55 +0530

    third commit

--- src/test/java/Regression/VerifyTitle.java
+++ src/test/java/Regression/VerifyTitle.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
+
 public class VerifyTitle {
 	
 	@Test
 	public void ClickEditTab(){
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://ci.datakonnect.com/");
+		Assert.assertTrue(driver.getTitle().contains("Anand"));
 		driver.quit();
 }}
